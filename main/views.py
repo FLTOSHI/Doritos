adf889445750e85d7d3017a529e9b3ce11ca77c3
FLTOSHI <fltoshimyname@gmail.com>
2024-10-17 19:25:44 +0300

    19-10H1 - сделал страницы: главная, контакты.

--- main/views.py
+++ main/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     context = {
+        'title': 'Главная страница',
         'products': ['Жвачка по рублю', 'Кока-коля', 'Чипсы «Лейс»'],
     }
     return render(request, 'main/index.html', context)
+
+def contacts(request):
+    context = {
+        'title': 'Контактная информация',
+    }
+    return render(request, 'main/contacts.html', context)
