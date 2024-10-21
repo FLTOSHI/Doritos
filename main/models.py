8704bf60d16a4f92ba7b18a1ff5c3dd3822e8c7d
Da <assasinstwo5@gmail.com>
2024-10-21 09:43:15 +0300

    2110W1 - Доработаны модели

--- main/models.py
+++ main/models.py
@@ -5,14 +5,21 @@ class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name="Наименование")
     description = models.CharField(max_length=255, verbose_name="Описание")
 
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.CharField(max_length=150, verbose_name='Описание')
-    preview = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, verbose_name="Изображение")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.IntegerField(max_length=5, verbose_name="Цена")
-    dateOfCreation = models.DateField(verbose_name="Дата создания")
-    dateOfLastEdit = models.DateField(verbose_name="Дата последнего изменения")
+    preview = models.ImageField(upload_to=None, height_field=None, width_field=None, verbose_name="Изображение")
+    category = models.ForeignKey('main.Category', on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.IntegerField(verbose_name="Цена")
+    created_at = models.DateField(verbose_name="Дата создания")
+    updated_at = models.DateField(verbose_name="Дата последнего изменения")
+    manufactured_at = models.DateField(verbose_name="Дата производства")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.Product = None
+        self.Category = None
 
     def __str__(self):
         return f'{self.Category} {self.Product}'
