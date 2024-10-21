b2689d6fa2b78dbc302a1e1e7ee442bbf6008aa7
Da <assasinstwo5@gmail.com>
2024-10-21 10:12:54 +0300

    2110W2 - Админ. панель

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from main import models
+
+
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'slug', 'is_published', 'created', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'stock', 'is_published', 'created', 'updated')
+    list_filter = ('created', 'updated', 'is_published', 'category')
+    list_editable = ('price', 'stock', 'is_published')
+    search_fields = ('name', 'description')
